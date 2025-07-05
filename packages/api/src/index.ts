4d1b597b393449373feaf737ff2e641edd29f231
Sahil <issahil404@gmail.com>
2025-07-06 01:24:22 +0530

    added google account support

--- packages/api/src/index.ts
+++ packages/api/src/index.ts
@@ -1,12 +1,31 @@
+import "dotenv/config";
 import Fastify from "fastify";
+import authRoutes from "./routes/auth";
+import {
+  serializerCompiler,
+  validatorCompiler,
+} from "fastify-type-provider-zod";
+import cors from "@fastify/cors";
 
 const fastify = Fastify({
   logger: true,
 });
 
+// Enable CORS for our web app
+fastify.register(cors, {
+  origin: "http://localhost:3000",
+});
+
+// Add Zod type provider
+fastify.setValidatorCompiler(validatorCompiler);
+fastify.setSerializerCompiler(serializerCompiler);
+
 fastify.get("/", async (request, reply) => {
   return { hello: "world" };
 });
+
+// Register auth routes
+fastify.register(authRoutes, { prefix: "/auth" });
 
 const start = async () => {
   try {
